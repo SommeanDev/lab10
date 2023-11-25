ca684c5980ccc612e75b379b87f7826833a7cdd7
Shivansh Mourya <96974868+SommeanDev@users.noreply.github.com>
2023-11-25 14:38:33 +0530

    added breeze files

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->integer('role_id')->default(2);
+            $table->integer('active')->default(1);
         });
     }
 
